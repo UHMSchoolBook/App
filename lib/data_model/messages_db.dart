848a0a595a36d47d071eb0e38502bfc11fd515f7
arminsoltan <arminsoltan1377@gmail.com>
2023-10-11 16:16:49 -1000

    (#23) Fix names and avatar

--- lib/data_model/messages_db.dart
+++ lib/data_model/messages_db.dart
@@ -14,11 +14,11 @@ class MessageData {
 
 class MessageDB {
   final List<MessageData> _messages = [
-    MessageData(username: 'User 1', text: 'Hello!', avatarUrl: 'https://placekitten.com/50/50'),
-    MessageData(username: 'User 2', text: 'Hi there!', avatarUrl: 'https://placekitten.com/49/49'),
-    MessageData(username: 'User 3', text: 'How are you?', avatarUrl: 'https://placekitten.com/48/48'),
-    MessageData(username: 'User 4', text: 'Good Morning!', avatarUrl: 'https://placekitten.com/47/47'),
-    MessageData(username: 'User 5', text: 'Have a nice day!', avatarUrl: 'https://placekitten.com/46/46'),
+    MessageData(username: 'Ali', text: 'Hello!', avatarUrl: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTONwt2K3MRTphbihJgxI8ASiZyCRi2fLdXBYW8lyzpzYnTnlaq'),
+    MessageData(username: 'Ben', text: 'Hi there!', avatarUrl: 'https://upload.wikimedia.org/wikipedia/commons/7/74/Benjamin_Netanyahu%2C_February_2023.jpg'),
+    MessageData(username: 'Brad', text: 'How are you?', avatarUrl: 'https://m.media-amazon.com/images/M/MV5BMjA1MjE2MTQ2MV5BMl5BanBnXkFtZTcwMjE5MDY0Nw@@._V1_FMjpg_UX1000_.jpg'),
+    MessageData(username: 'Shakira', text: 'Good Morning!', avatarUrl: 'https://hips.hearstapps.com/hmg-prod/images/shakira-2023-fashion-see-through-outfit-tiktok-641b0d2f0429c.jpeg?crop=0.324xw:0.248xh;0.434xw,0.0481xh&resize=1200:*'),
+    MessageData(username: 'Lana Del Rey', text: 'Have a nice day!', avatarUrl: 'https://pyxis.nymag.com/v1/imgs/4b9/fb9/74f8a3ed7f600b2081740ae60637ead7ee-lana-del-rey.rvertical.w330.jpg'),
   ];
 
   List<MessageData> get messages => _messages;
